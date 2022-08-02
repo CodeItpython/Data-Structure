6a9363b044767b044fc204188500e208bc488403
leeminseokdankook <32133660+LMS1229@users.noreply.github.com>
2022-08-02 15:21:09 +0900

    5주차 과제

---
+++ 과제/5주차/sample/sample.c
@@ -0,0 +1,25 @@
+#include "sample.h"
+
+void init(Heap* heap){
+
+}
+
+void push(Heap* heap, int data){
+
+}
+
+void pop(Heap* heap){
+
+}
+
+int peek(Heap* heap){
+
+}
+
+int isEmpty(Heap* heap){
+
+}
+
+void heap_sort(int* arr, int n){
+
+}
